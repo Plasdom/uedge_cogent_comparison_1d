4746b75d80869d962db41d21107ad270befe2052
Dominic Power <power8@llnl.gov>
2026-06-28 18:06:50 -0400

    Added cogent run

--- plot_comparison.py
+++ plot_comparison.py
@@ -8,7 +8,7 @@ import plots
 
 # Specify locations of both runs
 UEDGE_RUN = "ue1d.h5"
-COGENT_RUN = "../cogent_runs/no_particle_source_lopower_hidens"
+COGENT_RUN = "cogent_run"
 
 # Load UEDGE run
 hdf5_restore(UEDGE_RUN)
